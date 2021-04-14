2c093e914676ee8475a07e2b4a79347a0e4b7fa0
DaniilFomin322 <79974557+DaniilFomin322@users.noreply.github.com>
2021-04-15 01:49:42 +0300

    Домашнее задание №1: Базовые навыки работы с Selenium (#1)
    * Add files via upload
    
    Сделал все указанные тесты из домашнего задания №1: Базовые навыки работы с Selenium
    
    * повторяющийся код вынес в отдельные функции, избавился от хвостов в локаторах

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(r'C:\Users\Ekaterina\PycharmProjects\pythonProject\lection\chromedriver.exe')
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
